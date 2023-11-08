8c57bfe471537490ace0930da9256997e7ed073f
beratgulsoy <beratgulsoyy@gmail.com>
2023-11-09 02:05:06 +0300

    get and post data

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -50,18 +50,18 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('URL')
-      .then(res => {
+    fetch("http://localhost:8080/feed/posts", { mode: "cors" })
+      .then((res) => {
         if (res.status !== 200) {
-          throw new Error('Failed to fetch posts.');
+          throw new Error("Failed to fetch posts.");
         }
         return res.json();
       })
-      .then(resData => {
+      .then((resData) => {
         this.setState({
           posts: resData.posts,
           totalPosts: resData.totalItems,
-          postsLoading: false
+          postsLoading: false,
         });
       })
       .catch(this.catchError);
@@ -106,31 +106,39 @@ class Feed extends Component {
       editLoading: true
     });
     // Set up data (with image!)
-    let url = 'URL';
+    let url = "http://localhost:8080/feed/post";
+    let method = "POST";
     if (this.state.editPost) {
       url = 'URL';
     }
 
-    fetch(url)
-      .then(res => {
+    fetch(url, {
+      method: method,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: postData.title,
+        content: postData.content,
+      }),
+    })
+      .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Creating or editing a post failed!');
+          throw new Error("Creating or editing a post failed!");
         }
         return res.json();
       })
-      .then(resData => {
+      .then((resData) => {
         const post = {
           _id: resData.post._id,
           title: resData.post.title,
           content: resData.post.content,
           creator: resData.post.creator,
-          createdAt: resData.post.createdAt
+          createdAt: resData.post.createdAt,
         };
-        this.setState(prevState => {
+        this.setState((prevState) => {
           let updatedPosts = [...prevState.posts];
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
+              (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else if (prevState.posts.length < 2) {
@@ -140,17 +148,17 @@ class Feed extends Component {
             posts: updatedPosts,
             isEditing: false,
             editPost: null,
-            editLoading: false
+            editLoading: false,
           };
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         this.setState({
           isEditing: false,
           editPost: null,
           editLoading: false,
-          error: err
+          error: err,
         });
       });
   };
